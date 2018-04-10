8a0e84163dac6f4f23896fe3b2f920cf03cb6b9c
ceer006 <ceer006@gmail.com>
2018-04-10 17:32:42 +0300

    сделал отдельные функции на текст и гистограмму

--- js/stat.js
+++ js/stat.js
@@ -28,6 +28,18 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getRandomInRange = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var renderText = function (ctx, text, x, y) {
+  ctx.fillText(text, x, y);
+};
+
+var renderGraf = function (ctx, x, y, width, height) {
+  ctx.fillRect(x, y, width, height);
+};
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
@@ -35,8 +47,8 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 140, 20);
-  ctx.fillText('Список результатов:', 140, 40);
+  renderText(ctx, 'Ура вы победили!', 140, 20);
+  renderText(ctx, 'Список результатов:', 140, 40);
 
   var maxTime = getMaxElement(times);
 
@@ -44,17 +56,14 @@ window.renderStatistics = function (ctx, players, times) {
     var coordinateX = CLOUD_X + GAP_X + (BAR_WIDTH + GAP_X) * i;
     var coordinateY = (CLOUD_HEIGHT - TEXT_HEIGHT - (barHeight * times[i]) / maxTime);
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], coordinateX, CLOUD_Y + GAP_Y + FONT_GAP);
-    ctx.fillText(Math.round(times[i]), coordinateX, GAP_TIME + coordinateY);
+    renderText(ctx, players[i], coordinateX, CLOUD_Y + GAP_Y + FONT_GAP);
+    renderText(ctx, Math.round(times[i]), coordinateX, GAP_TIME + coordinateY);
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      var getRandomInRange = function (min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      }
       var getRandomInt = getRandomInRange(1, 9);
       ctx.fillStyle = 'rgba(0, 0, 255, 0.' + getRandomInt + ')';
     }
-    ctx.fillRect(coordinateX, CLOUD_Y + GAP_Y + coordinateY, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    renderGraf(ctx, coordinateX, CLOUD_Y + GAP_Y + coordinateY, BAR_WIDTH, (barHeight * times[i]) / maxTime);
   }
 };
